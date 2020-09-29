a0c227cb2437fba8eb1bfc9fd4bc31b5e76fd8d8
Penske Williano <penskewilliano@gmail.com>
2020-09-29 15:04:13 +0200

    Update to new Yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,47 +1,34 @@
-const clientId = 'I8NXeJgpi8o8_AWvZmOcMg';
-const secret = 'E9rmxHyXMbMJeit4SqXTgccgVKUSB5RUoQTJ792kgT2RW0V9AswRPdoFWJj3DT9p';
-let accessToken;
+const APIKey =
+  "JN7dBnBYPZpnlq2lcf9FSRvA2DzwaDRFvvAHiFH94P8bHdTvSyZAn1EYb0EGaXtC4lOHd-0qf2ogCRHPXuVvhrIgR3aeptWcrZAy-GvPiCPLZngyNwFAHMRp3DjFWXYx";
 
 const Yelp = {
-    getAccessToken() {
-        if (accessToken) {
-            return new Promise(resolve => resolve(accessToken));
-        }
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
-            method: 'POST'   
-        }).then(response => {
-            return response.json();
-        }).then(jsonResponse => {
-            accessToken = jsonResponse.access_token;
-        });
-    },
-    search(term, location, sortBy) {
-        return Yelp.getAccessToken().then(() => {
-            return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+  async search(term, location, sortBy) {
+    const response = await fetch(
+            `https://protected-taiga-17963.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+            {
+                
                 headers: {
-                    Authorization: `Bearer ${accessToken}`
-                }
-              });
-            }).then(response => {
-                return response.json();
-            }).then(jsonResponse => {
-                if (jsonResponse.businesses) {
-                    return jsonResponse.businesses.map(business => ({
-                            id: business.id,
-                            imageSrc: business.image_url,
-                            name: business.name,
-                            address: business.location.address1,
-                            city: business.location.city,
-                            state: business.location.state,
-                            zipCode: business.location.zip_code,
-                            category: business.categories[0].title,
-                            rating: business.rating,
-                            reviewCount: business.review_count,
-                            url: business.url
-                    }));
-                }
-            });
-    }
+                    Authorization: `Bearer ${APIKey}`,
+                },
+            }
+        );
+        const jsonResponse = await response.json();
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map((business) => ({
+                id: business.id,
+                imageSrc: business.image_url,
+                name: business.name,
+                address: business.location.address1,
+                city: business.location.city,
+                state: business.location.state,
+                zipCode: business.location.zip_code,
+                category: business.categories[0].title,
+                rating: business.rating,
+                reviewCount: business.review_count,
+                url: business.url,
+            }));
+        }
+  },
 };
 
 export default Yelp;
